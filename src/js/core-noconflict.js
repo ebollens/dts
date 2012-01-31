d994bd686ddd98153cada80e78a91a26b00ffd12
Eric Bollens <ebollens@ucla.edu>
2012-01-30 20:38:31 -0800

    Add dts definition without _ alias in core-noconflict.js

---
+++ src/js/core-noconflict.js
@@ -0,0 +1,13 @@
+/**
+ * Core definition of the dts object without the alias _. 
+ * 
+ * In most cases, core.js should be sufficient. However, in the event that a 
+ * library already uses _, then this version may be used instead.
+ * 
+ * @author      Eric Bollens
+ * @copyright   Copyright (c) 2010-11 UC Regents
+ * @license     BSD
+ * @version     20120130
+ */
+
+var dts = {};
