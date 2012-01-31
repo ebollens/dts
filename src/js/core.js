8293ee839f61a3b09f05a4c425daee0dcc5a0732
Eric Bollens <ebollens@ucla.edu>
2012-01-30 20:38:12 -0800

    Add dts definition with _ alias in core.js

---
+++ src/js/core.js
@@ -0,0 +1,10 @@
+/**
+ * Core definition of the dts object with the alias _.
+ * 
+ * @author      Eric Bollens
+ * @copyright   Copyright (c) 2010-11 UC Regents
+ * @license     BSD
+ * @version     20120130
+ */
+
+var dts = {}, _ = dts;
