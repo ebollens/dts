e670ef8f60a3fa0825bd6e938f1afbcda47801ae
Eric Bollens <ebollens@ucla.edu>
2012-01-31 14:27:07 -0800

    Set default domain for dts.federation

--- src/js/federation.php
+++ src/js/federation.php
@@ -11,7 +11,22 @@
  * @version     20120131
  */
 
-$domain = 'localhost';
+$domain = $_SERVER['HTTP_HOST'];
+
+if(($i = strpos($domain, '://')) !== false)
+    $domain = substr($domain, $i+3);
+elseif(substr($domain, 0, 2) == '//')
+    $domain = substr($domain, 2);
+
+if(($i = strpos($domain, '/')) !== false)
+    $domain = substr($domain, $i);
+
+if(($i = strpos($domain, ':')) !== false)
+    $domain = substr($domain, $i);
+
+if(substr($domain, 0, 1) == '.')
+    $domain = substr($domain, 1);
+
 $script = 'dts/www/passthru.php';
 
 echo 'dts.federation = { '
