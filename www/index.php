07c0aa5d41895b1f06691276f34c1b6c48692656
Eric Bollens <ebollens@ucla.edu>
2012-02-01 18:00:38 -0800

    Add index file as an example

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$url = 'http://localhost/dts/www';
+
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script type="text/javascript" src="<?php echo $url; ?>/js.php"></script>
+        <script type="text/javascript" src="<?php echo $url; ?>/federation.php"></script>
+    </head>
+    <body>
+    </body>
+</html>
